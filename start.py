8ae8170f46e4f6f7017784defb9ff8e9b10ad41e
BluTen <nayanshetty2006@gmail.com>
2021-01-27 16:46:49 +0530

    Optimized JSON data to be Arduino frendly

--- start.py
+++ start.py
@@ -1,3 +1,8 @@
 import json
+<<<<<<< HEAD
 with open("data.json", 'w') as fp:
+    json.dump(None, fp)
+=======
+with open("data.json") as fp:
     json.dump({"display": None}, fp)
+>>>>>>> parent of 15feb0f... nah again again
